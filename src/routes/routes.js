f2d87bc0a1f6f59a8a73c137cd53cc043dd1ad64
SylveCreep <khoasas@gmail.com>
2022-01-02 20:28:38 +0700

    master page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,23 +1,60 @@
-import {BrowserRouter as Router, Route, Navigate, Routes  } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Navigate,
+  Routes,
+} from "react-router-dom";
 import Login from "../features/login/login";
 import SignUp from "../features/signup/signup";
+import NavBar from "../features/layout/navbar/navbar";
+import * as React from "react";
 
+function Routings({ children, ...renderProps }) {
+  const isLogin = true;
+  return (
+    <>
+      {isLogin === true ? (
+        <AdminRouting>
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+          <Route
+            path="/dashboard"
+            element={
+              <>
+                <p>dashboard</p>
+              </>
+            }
+          />
+        </AdminRouting>
+      ) : (
+        <GlobalRouting>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </GlobalRouting>
+      )}
+    </>
+  );
+}
 
-function Routings(){
-    return (
+function AdminRouting({ children, ...props }) {
+  return (
+    <>
+      <NavBar />
+      <body>
         <Router>
-            <Routes>
-                <Route path="/" element={<Navigate to="/login" />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<SignUp />} />
-            </Routes>
+          <Routes>{children}</Routes>
         </Router>
-    )
+      </body>
+    </>
+  );
+}
+
+function GlobalRouting({ children, ...props }) {
+  return (
+    <Router>
+      <Routes>{children}</Routes>
+    </Router>
+  );
 }
 
 export default Routings;
-
-
-
-
-
